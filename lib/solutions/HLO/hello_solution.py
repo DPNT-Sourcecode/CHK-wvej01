069f1aaf653b48f3be80d7aae3213daabc582499
sbx_user1051 <sbx_user1051@169.254.36.181>
2024-03-31 07:57:27 +0000

    Sun Mar 31 07:57:27 UTC 2024

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -6,5 +6,3 @@ def hello(friend_name: str) -> str:
     if not isinstance(friend_name, str):
         raise ValueError("friend_name should by a type of string!")
     return "Hello, World!"
-
-
--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -4,4 +4,3 @@ def compute(x: int, y: int) -> int:
         if not 0 <= number <= 100:
             raise ValueError("Values should be a positive integer between 0-100")
     return x + y
-
