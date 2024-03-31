2c5f81d03efb913449038199a8fc762415aa6ae0
sbx_user1051 <sbx_user1051@169.254.36.181>
2024-03-31 07:48:48 +0000

    Sun Mar 31 07:48:48 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,7 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
-    raise NotImplementedError()
+def compute(x: int, y: int) -> int:
+    for number in [x, y]:
+        if not 0 >= number <= 100:
+            raise ValueError("Values should be a positive integer between 0-100")
+    return x + y
+
