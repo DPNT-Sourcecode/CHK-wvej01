21abb233c44ff8d8fcfc3948099a71c45cec2542
sbx_user1051 <sbx_user1051@169.254.36.181>
2024-03-31 07:51:48 +0000

    Sun Mar 31 07:51:48 UTC 2024

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,12 @@
 from solutions.SUM import sum_solution
-
+import pytest
 
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+    def wrong_numbers(self):
+        with pytest.raises(ValueError):
+            sum_solution.compute(-5, 2)
+    def wrong_numbers(self):
+        with pytest.raises(ValueError):
+            sum_solution.compute(-5, 1101)
