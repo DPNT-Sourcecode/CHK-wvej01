d23afcc25e4827e767d9ca1ca5467ecb466c7e86
sbx_user1051 <sbx_user1051@169.254.36.181>
2024-03-31 08:19:12 +0000

    Sun Mar 31 08:19:12 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,23 +3,6 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 from price_rate_card import price_data, offers_data
-from dataclasses import dataclass
-
-@dataclass
-class Item:
-    name: str
-    total_quantity: int
-    covered_by_offer: int
-    offers_sum: int
-    regular_sum: int
-    total_price: int 
-
-
-@dataclass
-class ShoppingCart:
-    items: list[Item]
-    total_price: int
-
 
 
 def calc_items_quantity(skus: str) -> dict:
@@ -30,9 +13,12 @@ def calc_items_quantity(skus: str) -> dict:
         dict_with_item_quantity[letter] = {"quantity": skus.count(letter)}
     return dict_with_item_quantity
 
-def apply_offers_price(dict_with_item_quantity: dict)
+def apply_offers_price(dict_with_item_quantity: dict):
+    for item_name, data in dict_with_item_quantity.items():
+        while data["qa"]
 
 
 def checkout(skus):
     print(skus)
     raise NotImplementedError()
+
