c6a1f15ed91d50302a0c41b292a09c89a14b3576
sbx_user1051 <sbx_user1051@169.254.36.181>
2024-03-31 08:07:12 +0000

    Sun Mar 31 08:07:12 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,6 +4,16 @@
 # skus = unicode string
 
 
+def calc_items_quantity(skus: str) -> dict:
+    dict_with_item_quantity = dict()
+    for letter in skus:
+        if letter in dict_with_item_quantity.keys():
+            continue
+        dict_with_item_quantity[letter] = skus.count(letter)
+    return dict_with_item_quantity
+
+def apply_
+
 def checkout(skus):
     print(skus)
     raise NotImplementedError()
--- test/solution_tests/CHK/test_sum.py
+++ test/solution_tests/CHK/test_sum.py
@@ -1,8 +1,10 @@
 from solutions.CHK import checkout_solution
 import pytest
 
-class TestSum():
-    def test_sum(self):
+class TestCheckout():
+    def test_checkout(self):
+
+        "AAAAAABBBBCCCC"
         assert checkout_solution.compute(1, 2) == 3
 
         with pytest.raises(ValueError):
@@ -10,3 +12,8 @@ class TestSum():
 
         with pytest.raises(ValueError):
             sum_solution.compute(5, 101)
+
+
+class TestQuantityCheck():
+    def test_q_check(self):
+        
