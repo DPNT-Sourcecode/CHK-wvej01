7270d0dacd86201c41b8d7e5138df913de2794fd
sbx_user1051 <sbx_user1051@169.254.36.181>
2024-03-31 08:34:12 +0000

    Sun Mar 31 08:34:12 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -23,15 +23,27 @@ def calc_items_quantity(skus: str) -> dict:
         data_dict[letter] = {"total_quantity": skus.count(letter)}
     return data_dict
 
-def apply_offers_price(data_dict: dict):
+def apply_offers_price(data_dict: dict) -> dict:
     for item_name, data in data_dict.items():
         data["quantity_to_calc"] = data["total_quantity"]
         offers = offers_data.get(item_name)
+        if offers is None:
+            continue
         for offer_quantity, offer_price in offers.items():
-            while int(data["quantity_to_calc"]) > offer_quantity:
+            while int(data["quantity_to_calc"]) >= offer_quantity:
                 data["quantity_to_calc"]-= offer_quantity
                 data["offers_price"] = data.get("offers_price", 0) + offer_price
     return data_dict
+
+
+def apply_regular_prices(data_dict: dict) -> dict:
+    for _, data in data_dict.items():
+        data["regular_price"] = price_data * data["quantity_to_calc"]
+        data["quantity_to_calc"] = 0
+    
+    for _, data in data_dict.items():
+        data["total_price"] = data["offers_price"] + data["regular_price"]
+
 
 
 def checkout(skus):
